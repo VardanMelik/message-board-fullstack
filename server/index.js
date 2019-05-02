fe032f53a769526a6502c24a57d3345dbbe8a5a8
eyemotion <eyemotiongamestudio@gmail.com>
2019-05-02 12:54:21 +0400

    Upload to git

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Morgan package for logger needs
+app.use(morgan('tiny'));
+app.use(cors());
+
+//Body Parser 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Full Stack Message Board!'
+    });
+});
+
+const port = process.env.port || 1234;
+app.listen(port, (req, res) => {
+    console.log('Server is running on port: ' + port);
+});
